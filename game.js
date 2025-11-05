1ff726043b749973e58bbf8ace3d8a6f670ad4f7
Adam <181141344+breezywideman@users.noreply.github.com>
2025-11-05 14:49:12 +0100

    Began work on game logic  and made the change from 'statements' to 'questions' in the json file.

--- game.js
+++ game.js
@@ -1,5 +1,65 @@
 addEventListener("DOMContentLoaded", (event) => {
 
+    let allCategories = "";
+    let selectedCategory = "";
+
+
+    // Get quiz data from localStorage
+
+    function getQuizData() {
+
+        const storedQuizData = localStorage.getItem("quizData")
+
+        if (storedQuizData) {
+            return JSON.parse(storedQuizData);
+        } else {
+            return {
+                selectedCategory: "",
+            }
+        }
+    }
+
+    // Set quiz data to localStorage
+
+    function setQuizData(quizData) {
+        const quizDataAsString = JSON.stringify(quizData);
+        localStorage.setItem("quizData", quizDataAsString)
+    }
+
+    // Fetch our JSON (questions.json)
+
+    async function getQuestions() {
+        try {
+            const response = await fetch("/data/questions.json");
+            const result = await response.json()
+            //    console.log(result)
+            allCategories = result.categories;
+            loadCategories();
+        } catch (error) {
+            console.error("Could not get the JSON", error);
+        }
+    }
+
+    getQuestions();
+
+    // Load our categories
+
+    function loadCategories() {
+        const selectElement = document.querySelector(".category-select__field");
+
+        selectElement.innerHTML = '<option value="">Browse...</option>';
+
+        allCategories.forEach(category => {
+            const optionElement = document.createElement("option");
+            optionElement.value = category.id;
+            optionElement.textContent = `${category.title} (${category.questions.length} questions)`;
+            selectElement.appendChild(optionElement);
+        });
+    }
+
+    setQuizData();
+
+
     // Is this a smart way to handle the changing between sections?
 
     function sectionScroller() {
@@ -28,34 +88,34 @@ addEventListener("DOMContentLoaded", (event) => {
     // Countdown after turn
     // How can we get this to only trigger after the slide has loaded?
 
-    function nextPayerCountdown() {
+    // function nextPayerCountdown() {
 
-        const countdown = document.querySelector(".screen__counter");
+    //     const countdown = document.querySelector(".screen__counter");
 
-        countdown.textContent = "5";
+    //     countdown.textContent = "5";
 
-        setTimeout(() => {
-            countdown.textContent = "4";
-        }, 1000);
+    //     setTimeout(() => {
+    //         countdown.textContent = "4";
+    //     }, 1000);
 
-        setTimeout(() => {
-            countdown.textContent = "3";
-        }, 2000);
+    //     setTimeout(() => {
+    //         countdown.textContent = "3";
+    //     }, 2000);
 
-        setTimeout(() => {
-            countdown.textContent = "2";
-        }, 3000);
+    //     setTimeout(() => {
+    //         countdown.textContent = "2";
+    //     }, 3000);
 
-        setTimeout(() => {
-            countdown.textContent = "1";
-        }, 4000);
+    //     setTimeout(() => {
+    //         countdown.textContent = "1";
+    //     }, 4000);
 
-        setTimeout(() => {
-            countdown.textContent = "Go!";
-        }, 5000);
-    };
+    //     setTimeout(() => {
+    //         countdown.textContent = "Go!";
+    //     }, 5000);
+    // };
 
-    nextPayerCountdown();
+    // nextPayerCountdown();
 
 });
 
