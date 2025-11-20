76caa34bf19027d6614074c0b8cd55316898ff70
Begimai <babisatarova@gmail.com>
2025-11-20 14:15:43 +0100

    implement ready screen

--- src/js/ready.js
+++ src/js/ready.js
@@ -18,8 +18,6 @@ let playerName;
 
 export function initReady() {
   // STEP 1: Find the HTML elements you need
-  //  const greetingText = document.querySelector("[data-screen="ready"] .screen__title__text--greeting");
-  //  const playerNameText = document.querySelector("[data-screen="ready"] .screen__title__text--username");
 
   greetingText = document.querySelector(
     '[data-screen="ready"] .screen__title__text--greeting'
@@ -28,8 +26,6 @@ export function initReady() {
     '[data-screen="ready"] .screen__title__text--username'
   );
 
-  console.log("initReady: greetingText, playerName:", greetingText, playerName);
-
   // Expose update function so navigation.js can call it automatically
   window.updateReadyScreen = updateReadyScreen;
 }
@@ -37,37 +33,25 @@ function updateReadyScreen() {
   console.log("updateReadyScreen running - start");
 
   // STEP 2: Get random greeting message
-  // Get the array: gameState.questionsData.responseMessages.readyMessage
-  // Use getRandomFromArray() to pick one random message
-  if (gameState.questionsData === null) return;
+
+  if (gameState.questionsData === null) {
+    console.error("No questions data");
+    return;
+  }
 
   const maybeMessages = gameState.questionsData.responseMessages.readyMessage;
 
   if (maybeMessages) {
-    console.warn(
-      "Ready messages not found in gameState.guestionsData.responseMessages.readyMessage - using fallback"
-    );
-
     const randomGreeting = getRandomFromArray(maybeMessages);
 
     // STEP 3: Get current player"s name
-    // If gameState.currentPlayerIndex is 0, use gameState.playerOne
-    // If gameState.currentPlayerIndex is 1, use gameState.playerTwo
+
     const idx = gameState?.currentPlayerIndex;
     const currentPlayerName =
-      idx === 0
-        ? gameState?.playerOne || "Player 1"
-        : gameState?.playerTwo || "Player 2";
-    console.log(
-      "updateReadyScreen: idx =",
-      idx,
-      ", currentPlayer =",
-      currentPlayerName
-    );
+      idx === 0 ? gameState.playerOne : gameState.playerTwo;
 
     // STEP 4: Update the screen
-    // Set greetingText to: randomGreeting + ","
-    // Set playerNameText to: currentPlayerName
+
     if (!greetingText || !playerName) {
       console.warn("One of DOM elements is null:", {
         greetingText,
@@ -78,9 +62,5 @@ function updateReadyScreen() {
 
     greetingText.textContent = randomGreeting + ",";
     playerName.textContent = currentPlayerName;
-
-    console.log("ready screen updated:", { randomGreeting, currentPlayerName });
-  } else {
-    console.log("no maybe messages");
   }
 }
